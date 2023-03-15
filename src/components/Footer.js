dffffd14abbd58c9d168be4da582b2f775c258b6
TiagoIesbick <tiagoiesbick@gmail.com>
2023-03-15 17:35:23 -0300

    footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,36 @@
 import logo from '../assets/images/logo_green.png';
 import Nav from "./Nav";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 
 export default function Footer() {
     return (
         <footer>
-            <img src={logo} alt="logo" className='logo'/>
-            <div>
-                Doormat Navigation
-                <Nav />
-            </div>
-            <div>
-                Contact
-                <ul>
-                    <li><a href="/">Address</a></li>
-                    <li><a href="tel:+5551997912221">XXXXX</a></li>
-                    <li><a href="mailto:littlelemon@littlelemon.com">Email</a></li>
-                </ul>
-            </div>
-            <div>
-                Social Media Links
-                <ul>
-                    <li><a href="/">Facebook</a></li>
-                    <li><a href="/">Instagram</a></li>
-                    <li><a href="/">Twitter</a></li>
-                </ul>
+            <div className='footer-container'>
+                <div className='footer-logo'>
+                    <img src={logo} alt="logo" className='logo'/>
+                </div>
+                <div>
+                    <span>Doormat Navigation</span>
+                    <Nav />
+                </div>
+                <div className='contact'>
+                    <span>Contact</span>
+                    <ul>
+                        <li><a href="https://maps.google.com/?q=Chicago"><FontAwesomeIcon icon={solid('location-dot')} /></a></li>
+                        <li><a href="tel:+1408XXXXXXX"><FontAwesomeIcon icon={solid('phone')} /></a></li>
+                        <li><a href="mailto:littlelemon@littlelemon.com"><FontAwesomeIcon icon={solid('envelope')} /></a></li>
+                    </ul>
+                </div>
+                <div className='social-media-links'>
+                    <span>Social Media</span>
+                    <ul>
+                        <li><a href="/"><FontAwesomeIcon icon={brands('facebook')}/></a></li>
+                        <li><a href="/"><FontAwesomeIcon icon={brands('instagram')}/></a></li>
+                        <li><a href="/"><FontAwesomeIcon icon={brands('twitter')}/></a></li>
+                    </ul>
+                </div>
             </div>
         </footer>
     );
